410ef2bb54ac81f1f3a33bd8b5c5233120bd1cd5
BlinkWork <86482353+BlinkWork@users.noreply.github.com>
2023-07-11 08:39:02 +0700

    Update UserUpdateServlet.java

--- src/java/controller/UserUpdateServlet.java
+++ src/java/controller/UserUpdateServlet.java
@@ -25,6 +25,17 @@ import model.User;
 public class UserUpdateServlet extends HttpServlet {
 
     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String id = request.getParameter("userId");
+        UserDAO u = new UserDAO();
+        request.setAttribute("user", u.getUserByID(id));
+        request.getRequestDispatcher("views/userEdit.jsp").forward(request, response);
+    }
+    
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
